654c5ea2d17447e2024ffa375e80f14a5ccbf3c9
Lucas Fraguaga <lucasfraguaga@live.com>
2024-07-10 21:32:11 -0300

    nivelado

--- Sistema de clima/DAL/Conexion.cs
+++ Sistema de clima/DAL/Conexion.cs
@@ -14,7 +14,7 @@ namespace DAL
         {
             var cs = new SqlConnectionStringBuilder();
             cs.IntegratedSecurity = true;
-            cs.DataSource = @"DESKTOP-6S00J44\SQLEXPRESS01";
+            cs.DataSource = @"LACUCA\SQLEXPRESS";
             cs.InitialCatalog = "web";
             return cs.ConnectionString;
         }
